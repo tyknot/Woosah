ff5d405c916d9e4e68d2a0c7d0ff6b316a8879b9
tyknot <tyknott94@gmail.com>
2018-07-04 09:10:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,14 +13,14 @@ function color(){
 	document.body.style.backgroundColor = newColor;
 }
 
-window.onload = setInterval(color, 10000);
+//window.onload = setInterval(color, 10000);
 
 // everything audio related
-//const volSlider = document.querySelector("#vol-slider");
-const volSlider = document.getElementsByTagName("input");
-const icons = document.querySelectorAll("svg");
 const audioEl = document.querySelectorAll("audio");
-
+const icons = document.querySelectorAll("#noise-icon");
+const volEl = document.querySelectorAll("#vol-slider");
+const vol_on = document.querySelector(".vol-on");
+const vol_off = document.querySelector(".vol-off");
 const rndom_btn = document.querySelectorAll(".random-btn");
 const prod_btn = document.querySelector(".productivity-btn-btn");
 const relax_btn = document.querySelector(".relax-btn");
@@ -29,19 +29,20 @@ const audio_class = {rain:"rain",storm:"storm",
 			activity:"activity",clock:"clock",
 			truck:"truck",users:"users"};
 
-// icon click control
+// main volume control
 document.addEventListener("click", function(e){
 	let target = e.target;
-
-	// main volume click
+	// mute audio click
 	if(target.matches(".main-vol")){
 		vol_on.classList.toggle("hide-vol");
 		vol_off.classList.toggle("show-vol");
 		for(let i = 0; i < audioEl.length; i++){
+			// vol off
 			if(vol_on.classList.contains("hide-vol")){
 				vol_off.classList.add("active-opacity");
 				audioEl[i].classList.add("muted");
 				audioEl[i].pause();
+			// vol on	
 			}else if(icons[i].classList.contains("active-opacity")){
 				audioEl[i].classList.remove("muted");
 				audioEl[i].play();
@@ -51,7 +52,7 @@ document.addEventListener("click", function(e){
 			}
 		}
 	}
-	
+
 	// icon click
 	if(target.matches("#noise-icon")){
 		const br = target.nextElementSibling;
@@ -70,37 +71,40 @@ document.addEventListener("click", function(e){
 		}else{
 			audio.pause();
 		}
-		
+
+	}
+
 	// random btn click
 	if(target.matches(".random-btn")){
-		for(let i = 0; i < 3; i++){
+		for(let i = 0; i < 2; i++){
 			try{
 				const rand_icon = icons[Math.round(Math.random() * icons.length)];
 				const br = rand_icon.nextElementSibling;
-				console.log(br);
 				const vol = br.nextElementSibling;
 				const audio = rand_icon.parentNode.childNodes[1];
-				console.log(rand_icon.className);
+			
 				rand_icon.classList.add("random");
 				rand_icon.classList.add("active-opacity");
 				vol.classList.toggle("show-vol");
-				if(vol.classList.contains("show-vol") && rand_icon.classList.contains("active-opacity") && rand_icon.classList.contains("random")){
+				console.log(rand_icon.className);
+				if(audio.classList.contains("muted")){
+					audio.pause();
+				}else if(vol.classList.contains("show-vol") && rand_icon.classList.contains("active-opacity") && rand_icon.classList.contains("random")){
 					audio.play();
 				}else{
 					rand_icon.classList.remove("active-opacity");
 					rand_icon.classList.remove("random");
 					audio.pause();
 				}
-				if(i > 3){
-					i -= 1;
-				}
+				
 			}catch(error){}
+
 		}
 	}
-		
+
 	// productivity btn click
 	if(target.matches(".productivity-btn")){
-		for(let i = 0; i < 3; i++){
+		for(let i = 0; i < 2; i++){
 			try{
 				const icon = icons[Math.round(Math.random() * icons.length)];
 				const br = icon.nextElementSibling;
@@ -110,7 +114,9 @@ document.addEventListener("click", function(e){
 					console.log(icon.className);
 					icon.classList.add("active-opacity");
 					vol.classList.toggle("show-vol");
-					if(vol.classList.contains("show-vol") && icon.classList.contains("active-opacity")){
+					if(audio.classList.contains("muted")){
+						audio.pause();
+					}else if(vol.classList.contains("show-vol") && icon.classList.contains("active-opacity")){
 						audio.play();
 					}else{
 						icon.classList.remove("active-opacity");
@@ -120,12 +126,31 @@ document.addEventListener("click", function(e){
 			}catch(error){}
 		}
 	}
-		
+
 	// relax btn click
 	if(target.matches(".relax-btn")){
-		
+		for(let i = 0; i < 2; i++){
+			try{
+				const icon = icons[Math.round(Math.random() * icons.length)];
+				const br = icon.nextElementSibling;
+				const vol = br.nextElementSibling;
+				const audio = icon.parentNode.childNodes[1];
+				if(icon.classList.contains("relax")){
+					console.log(icon.className);
+					icon.classList.add("active-opacity");
+					vol.classList.toggle("show-vol");
+					if(audio.classList.contains("muted")){
+						audio.pause();
+					}else if(vol.classList.contains("show-vol") && icon.classList.contains("active-opacity")){
+						audio.play();
+					}else{
+						icon.classList.remove("active-opacity");
+						audio.pause();
+					}
+				}
+			}catch(error){}
+		}
 	}
-
 });	
 
 // volume control
